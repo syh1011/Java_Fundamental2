eba230423c51d9a3408a8762cdd3b0e329689ae1
syh1011 <syh@hbilab.org>
2021-05-10 17:52:23 +0900

    2021-05-10-v6

---
+++ src/java_20210510/CalendarDemo.java
@@ -0,0 +1,11 @@
+package java_20210510;
+
+public class CalendarDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar c = new Calendar(2021,5,10);
+		c.print();
+	}
+
+}
