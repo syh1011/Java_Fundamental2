00bdd98027e5a394cd836874598d5567420a504d
syh1011 <syh@hbilab.org>
2021-05-12 09:00:44 +0900

    2021-05-11-v7

--- src/java_20210511/ShapeDemo.java
+++ src/java_20210511/ShapeDemo.java
@@ -3,8 +3,15 @@ package java_20210511;
 public class ShapeDemo {
 	public static void main(String[] args) {
 		ShapeManager manager = ShapeManager.getInstance();
-		
 		Circle t = new Circle();
 		manager.run(t);
+		
+		String str ="hello";
+		String str1 = "hello";
+		String str3 = new String("hello");
+		String str4 = new String("hello");
+		System.out.println(str==str1);
+		System.out.println(str3==str4);
+		System.out.println(str==str4);
 	}
 }
