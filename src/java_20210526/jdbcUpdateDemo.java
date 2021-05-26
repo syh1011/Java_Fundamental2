17e6986441084d79345a82d454de25f5c0f56f3b
syh1011 <syh@hbilab.org>
2021-05-26 11:36:46 +0900

    2021-05-26-v2

--- src/java_20210526/jdbcUpdateDemo.java
+++ src/java_20210526/jdbcUpdateDemo.java
@@ -5,61 +5,54 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class JdbcInsertDemo {
+public class jdbcUpdateDemo {
 	public static void main(String[] args) {
+		//1. 드라이브 로드한다.
 		
-		//1. 드라이버 로드한다.
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
-			System.out.println("드라이버로드 성공");
 		} catch (ClassNotFoundException e) {
-			System.err.println(
-					"드라이버로드 실패 : "+e.getMessage());
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		try {
-			//2. 데이터 베이스와 연결하는 Connection 객체를 생성한다.
+			//2. 데이터베이스와 연결할 수 있는 Connection 객체를 생성
 			con = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/kpc",//url
-					"kpc12",//user
-					"kpc1212");//password
-			System.out.println("데이터베이스 연결 성공");
+					"jdbc:mysql://localhost/kpc",
+					"kpc12","kpc1212");
 			
 			//3. SQL문을 전송할 수 있는 PreparedStatement 객체를 생성
-			
 			StringBuilder sql = new StringBuilder();
-			sql.append("INSERT INTO member(num, NAME, addr) ");
-			sql.append("VALUES (?,?,?)");
-			
+			sql.append("UPDATE member ");
+			sql.append("SET NAME =?, addr =?  ");
+			sql.append("WHERE num = ? ");
 			pstmt = con.prepareStatement(sql.toString());
-			//4. 바인딩 변수 설정하기
-			pstmt.setInt(1, 11);
-			pstmt.setString(2, "두테르테");
-			pstmt.setString(3, "필리핀");
+			
+			//4. 바인딩 변수를 설정한다.
+			int index = 1;
+			pstmt.setString(index++, "두테르테2");
+			pstmt.setString(index++, "필리핀2");
+			pstmt.setInt(index, 11);
 			
 			//5. SQL문을 전송한다.
 			int resultCount = pstmt.executeUpdate();
 			System.out.println("갱신된 행의 수 : "+resultCount);
 			
-			
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
+			
 			try {
+				//6. 모든자원을 반납한다.
 				if(con != null) con.close();
-				if(pstmt != null) pstmt.close();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
-		
-		
 		
 		
 		
